bbd63c53f6c71a5b086426617b74aad9a1c14edf
lawal thywill <lawalkanyinsola889@gmail.com>
2025-11-20 12:12:58 -0800

    first python file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+sum = number1 + number2
+print("The sum of", number1, "and", number2, "is:", sum)
+difference = number1 - number2
+print("The difference when", number2, "is subtracted from", number1, "is:", difference)
+product = number1 * number2
+print("The product of", number1, "and", number2, "is:", product)
