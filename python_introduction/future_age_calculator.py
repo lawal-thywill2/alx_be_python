a9bacb374eb70fde1564336de1118b88933e0741
lawal thywill <lawalkanyinsola889@gmail.com>
2025-11-20 13:17:11 -0800

    variable corrected

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
-current_age = int(input("Enter your current age: "))
+current_age = int(input("How old are you? "))
 years_to_add = 27
 future_age = current_age + years_to_add
-print("In 2050, you will be:", future_age, "years old.") 
+print("In 2050, you will be", future_age, "years old.") 
