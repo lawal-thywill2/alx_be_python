fafaece9581324171abcc2c38cf6efc510d2e4e3
lawal thywill <lawalkanyinsola889@gmail.com>
2025-11-20 12:34:09 -0800

    second file executed

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+principal = 1000 # representing $1000
+rate = 0.05    # representing 5% annual interest rate 
+time = 3       # representing 3 years 
+interest = principal * rate * time
+print("the product of", principal, ",", rate, "and", time, "is", interest) 
