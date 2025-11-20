b6ba07bb8ea1d65c89ca45ab1e4033ba379fd493
lawal thywill <lawalkanyinsola889@gmail.com>
2025-11-20 13:19:35 -0800

    first file executed

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,7 +1,5 @@
 number1 = 10
 number2 = 5
-number1 = int(input("Enter the first number: "))
-number2 = int(input("Enter the second number: "))
 sum = number1 + number2
 print("The sum of", number1, "and", number2, "is:", sum)
 difference = number1 - number2
