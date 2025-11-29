caa4f5fb18dca67320009fb9d0fd6832e3c5a349
lawal thywill <lawalkanyinsola889@gmail.com>
2025-11-29 07:22:40 -0800

    correct function is defined

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation () :
+def perform_operation (num1, num2, operation):
     """Performs a simple arithmetic operation and returns the result."""
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
@@ -16,5 +16,5 @@ def perform_operation () :
             return "Error: Division by zero is not allowed."
     else:
         return "Error: Invalid operation."
-result = perform_operation()
+result = perform_operation('num1', 'num2', 'operation')
 print(f"Result: {result}")
