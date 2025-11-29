2230190bb79121a78e707ee562694c55c27f2038
lawal thywill <lawalkanyinsola889@gmail.com>
2025-11-29 07:30:46 -0800

    .

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,10 +2,10 @@ def perform_operation(num1, num2, operation):
     """Performs a simple arithmetic operation and returns the result."""
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-    operation =input("Enter the operation (add,substract,multiply,divide): "). strip().lower()
+    operation =input("Enter the operation (add,subtract,multiply,divide): "). strip().lower()
     if operation == "add":
         return num1 + num2
-    elif operation == "substract":
+    elif operation == "subtract":
         return num1 - num2
     elif operation == "multiply":
         return num1 * num2
