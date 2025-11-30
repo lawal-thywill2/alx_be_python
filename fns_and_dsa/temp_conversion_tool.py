10ed955f66e887d354e40fadf093874dfb42db51
lawal thywill <lawalkanyinsola889@gmail.com>
2025-11-30 10:39:56 -0800

    Mistake corrected

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,14 +1,14 @@
 temperature = float(input("Enter the temperature to convert: "))
 scale = input("Is this temperature in Celsius or Fahrenheit? (C/F) ").strip().upper()
-FAHRENHEIT_TO_CELSIUS = (temperature - 32) * 5.0/9.0
-CELSIUS_TO_FAHRENHEIT = (temperature * 9.0/5.0) + 32
+FAHRENHEIT_TO_CELSIUS_FACTOR = (temperature - 32) * 5.0/9.0
+CELSIUS_TO_FAHRENHEIT_FACTOR = (temperature * 9.0/5.0) + 32
 def convert_to_celsius(fahrenheit):
-    print(f"{temperature}""°F" " is " f"{FAHRENHEIT_TO_CELSIUS}""°C")
+    print(f"{temperature}""°F" " is " f"{FAHRENHEIT_TO_CELSIUS_FACTOR}""°C")
 def convert_to_fahrenheit(celsius):
-    print(f"{temperature}""°C" " is " f"{CELSIUS_TO_FAHRENHEIT}""°F")
+    print(f"{temperature}""°C" " is " f"{CELSIUS_TO_FAHRENHEIT_FACTOR}""°F")
 if scale == 'C':
     convert_to_fahrenheit(temperature)
 elif scale == 'F':
-    convert_to_celsius(temperature)
+     convert_to_celsius(temperature)
 else:
     print("Invalid temperature. please input a numeric value.")
