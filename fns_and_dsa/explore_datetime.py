201cdfcbc53f2bc2e59e7d3f181ea680c86865a3
lawal thywill <lawalkanyinsola889@gmail.com>
2025-11-30 09:51:14 -0800

    mistake corrected in variable

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -4,7 +4,7 @@ def display_current_datetime():
     current_date = datetime.datetime.now()
     return current_date.strftime("Current date and time: %Y-%m-%d %H:%M:%S")
 print(f"{display_current_datetime()}")
-timedelta = int(input("Enter number of days to add to the current date: "))
+timedelta =int(input("Enter the number of days to add to the current date: "))
 def calculate_future_date(days):
     future_date = datetime.datetime.now() + datetime.timedelta(days=days)
     return future_date.strftime("Future date: %Y-%m-%d %H:%M:%S")
