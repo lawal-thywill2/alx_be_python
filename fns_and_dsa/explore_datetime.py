b59df49bfcfe7af0e14d2c3ad42c0931dd868aea
lawal thywill <lawalkanyinsola889@gmail.com>
2025-11-30 09:41:28 -0800

    third function file executed

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,10 @@
+import datetime  
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    return current_date.strftime("Current date and time: %Y-%m-%d %H:%M:%S")
+print(f"{display_current_datetime()}")
+timedelta = int(input("Enter number of days to add to the current date: "))
+def calculate_future_date(days):
+    future_date = datetime.datetime.now() + datetime.timedelta(days=days)
+    return future_date.strftime("Future date: %Y-%m-%d %H:%M:%S")
+print(f'{calculate_future_date(timedelta)}')
