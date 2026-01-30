4c32a164b7c0875a4cadb585528a7b3d59801217
lawal thywill <lawalkanyinsola889@gmail.com>
2026-01-30 07:12:20 -0800

    FINALLLL CHANGES

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -10,6 +10,6 @@ def main():
     denominator = sys.argv[2]
 
     result = safe_divide(numerator, denominator)
-    print(result)
+    print(f"The result of the division is {result}")
 if __name__ == "__main__":
     main()
