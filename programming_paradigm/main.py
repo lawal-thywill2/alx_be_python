a0db26e020d4e8334c4a503b88bf1176a66a6862
lawal thywill <lawalkanyinsola889@gmail.com>
2026-01-30 05:57:43 -0800

    second OOP FILE

---
+++ programming_paradigm/main.py
@@ -0,0 +1,11 @@
+class  safe_divide:
+    @staticmethod
+    def safe_divide(numerator, denominator):
+        try:
+            num = float(numerator)
+            denom = float(denominator)
+            if denom == 0:
+                return "Error: Cannot divide by zero."
+            return num / denom
+        except ValueError:
+            return "Error: Please enter numeric values only."
