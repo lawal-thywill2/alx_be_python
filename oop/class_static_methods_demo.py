0e10d668fb6ecd1c0333401234085d62915f687e
lawal thywill <lawalkanyinsola889@gmail.com>
2026-01-31 15:41:08 -0800

    last file

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,10 @@
+class Calculator:
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+    
+    @classmethod
+    def multiply(cls, a, b):
+        return a * b
